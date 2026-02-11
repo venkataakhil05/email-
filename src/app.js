4df03b37656ea73090a67057ad84772d86fb0e81
Akhil <venkataakhil05@gmail.com>
2026-02-11 10:25:44 +0530

    Auto-update

--- src/app.js
+++ src/app.js
@@ -9,7 +9,6 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
-console.log("Mounting /api/artifacts...");
 app.use("/api/artifacts", artifactRoutes);
 
 export default app;
